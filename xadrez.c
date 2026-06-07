687a2247f8e51af5ab0b38b3ceb99588abfdb626
Mageske <mageskeesantana@gmail.com>
2026-06-07 01:24:32 +0000

    Desafio: nível aventureiro Movimentando o Cavalo

--- xadrez.c
+++ xadrez.c
@@ -26,5 +26,27 @@ int main() {
     } while (casasRainha <= 8);
     printf("\n");
 
+    // --- MOVIMENTAÇÃO DO CAVALO (NÍVEL AVENTUREIRO) ---
+    printf("--- Movimento do Cavalo ---\n");
+
+    int jogadaVertical = 2;
+    int jogadaHorizontal = 1;
+
+    // Loop externo (for) controla o avanço nas linhas (vertical)
+    for (int i = 1; i <= jogadaVertical; i++) {
+        printf("Baixo\n");
+
+        // Quando o Cavalo atinge a segunda casa para baixo, o loop interno executa
+        if (i == jogadaVertical) {
+            int j = 1;
+            // Loop interno (while) controla o avanço na perpendicular (esquerda)
+            while (j <= jogadaHorizontal) {
+                printf("Esquerda\n");
+                j++;
+            }
+        }
+    }
+    printf("\n");
+
     return 0;
 }
